14b017e8bf801dcfdd8a044e8544a627c08ab607
TurboGoose <Ilya23like73@yandex.ru>
2018-04-30 21:45:37 +0300

    Fix task 1 (№2)

--- hash_tools.py
+++ hash_tools.py
@@ -2,7 +2,16 @@ from hashlib import md5
 
 
 def check_hash(hash):
-    return md5(hash.encode("utf8")).hexdigest().startswith("00000")
+    try:
+
+        user_id, string = hash.split("-", maxsplit=1)
+        if not user_id.isdigit():
+            return False
+
+        return md5(hash.encode("utf8")).hexdigest().startswith("00000")
+
+    except ValueError:
+        return False
 
 
 def check_hashes(strings):
